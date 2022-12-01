2541fdc227b1f2caee8548f07f6fa5ae0fdbc957
David A. Thatcher <david.austin.thatcher@gmail.com>
2022-12-01 12:38:08 +0000

    modified:   pages/software_delivery.py

--- pages/software_delivery.py
+++ pages/software_delivery.py
@@ -10,10 +10,6 @@ st.title("Research Exchange")
 st.header("Software Delivery Performance")
 st.markdown("##")
 
-col1, col2 = st.columns(2)
-col1.metric("", "Throughput", "Trend")
-col2.metric("", "Stability", "Trend")
-
 df1 = pd.read_excel(
        io='testPython.xlsx',
        engine='openpyxl',
@@ -22,18 +18,53 @@ df1 = pd.read_excel(
        usecols='A:C',
        nrows=4,
 )
-def get_data_from_excel():
+def get_data_from_excel_df():
     df = pd.read_excel(
         io="testPython.xlsx",
         engine="openpyxl",
         sheet_name="Test",
        #skiprows=3,
-        usecols="D",
+        usecols="D:E",
         nrows=6,
     )
     
     return df
 
+def get_data_from_excel_lt():
+    df3 = pd.read_excel(
+        io="testPython.xlsx",
+        engine="openpyxl",
+        sheet_name="Test",
+       #skiprows=3,
+        usecols="F:G",
+        nrows=6,
+    )
+    
+    return df3
+
+def get_data_from_excel_mttr():
+    df4 = pd.read_excel(
+        io="testPython.xlsx",
+        engine="openpyxl",
+        sheet_name="Test",
+       #skiprows=3,
+        usecols="H",
+        nrows=6,
+    )
+    
+    return df4
+
+def get_data_from_excel_cfr():
+    df5 = pd.read_excel(
+        io="testPython.xlsx",
+        engine="openpyxl",
+        sheet_name="Test",
+       #skiprows=3,
+        usecols="I",
+        nrows=6,
+    )
+    
+    return df5
 # ----- SIDEBAR -------
 #st.sidebar.header("Please Filter Here")
 #freq = st.sidebar.multiselect(
@@ -46,10 +77,35 @@ def get_data_from_excel():
 
 st.dataframe(df1)
 
+col3, col4 = st.columns(2)
+col3.metric("", "MTTR", "-Last 30 Days", delta_color="inverse")
+col4.metric("", "Change Failure Rate", "-Last 30 Days", delta_color="off")
+
 #st.title("Deployments (Last 4 Weeks)")
-st.header("# Deployments (Last 4 Weeks)")
+st.header("# Deployments")
 st.markdown("""---""")
 
-df = get_data_from_excel()
+st.metric("Total Oct. Deployments vs Sep.", "15 Deployments", "-1 delta")
+
+df = get_data_from_excel_df()
 st.line_chart(df)
 
+st.header("Lead Time for Change (avg days)")
+st.markdown("""---""")
+
+st.metric("Total Oct. avg Lead Time vs Sep", "44 Days", "-9", delta_color="inverse")
+
+df2 = get_data_from_excel_lt()
+st.line_chart(df2)
+
+st.header("MTTR (hours)")
+st.markdown("""---""")
+
+df3 = get_data_from_excel_mttr()
+st.line_chart(df3)
+
+st.header("Change Failure Rate")
+st.markdown("""---""")
+
+df4 = get_data_from_excel_cfr()
+st.line_chart(df4)
