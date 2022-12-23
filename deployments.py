bc250c292ca540c76286b94ec9195819cba71292
David A. Thatcher <david.austin.thatcher@gmail.com>
2022-12-23 20:20:03 +0000

    modified:   deployments.csv 	modified:   deployments.py

--- deployments.py
+++ deployments.py
@@ -1,48 +1,30 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# Read in the CSV file
-@st.cache
-def read_data(file):
-    df = pd.read_csv(file)
-    df['deployment_date'] = pd.to_datetime(df['deployment_date'])
-    return df
+# Prompt the user to upload a CSV file
+uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 
-# Calculate the deployment frequency according to DORA's method
-def calculate_frequency(df):
-    delta = df['deployment_date'].max() - df['deployment_date'].min()
-    days = delta.days + 1
-    if days >= 365:
-        frequency = 'Yearly'
-    elif days >= 30:
-        frequency = 'Monthly'
-    elif days >= 7:
-        frequency = 'Weekly'
+# If a file is uploaded, read it into a Pandas DataFrame
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file, parse_dates=["deployment date"], infer_datetime_format=True)
+
+    # Prompt the user to select an application
+    application = st.selectbox("Select an application:", df["Application"].unique())
+
+    # Filter the DataFrame to only include rows for the selected application
+    df = df[df["Application"] == application]
+
+    # Calculate the deployment frequency
+    deployment_counts = df["deployment date"].dt.week.value_counts()
+    if (deployment_counts >= 3).any():
+        frequency = "Daily"
+    elif (deployment_counts >= 3).sum() >= 3:
+        frequency = "Weekly"
     else:
-        frequency = 'Daily'
-    return frequency
+        frequency = "Monthly"
 
-# Plot a trend line of the data
-def plot_trend(df):
-    plt.plot(df['deployment_date'], df['deployments'])
-    plt.xlabel('Date')
-    plt.ylabel('Number of Deployments')
-    plt.title('Trend of Deployments Over Time')
+    # Display the frequency in a text field
+    st.write(f"Deployment frequency: {frequency}")
 
-# Main function
-def main():
-    # Select the CSV file to load
-    file = st.file_uploader('Upload a CSV file of deployment data:')
-    if file is not None:
-        df = read_data(file)
-
-        # Calculate the deployment frequency
-        frequency = calculate_frequency(df)
-        st.write(f'The deployment frequency is: {frequency}')
-
-        # Plot the trend line
-        st.plotly_chart(plot_trend(df))
-
-if __name__ == '__main__':
-    main()
+    # Display the selected application's data in a table
+    st.dataframe(df)
